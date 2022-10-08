ac7ab2dd848bab336f0170ecb8fce6645c29ce75
camilorendon <juancamilo.g.0512@gmail.com>
2022-10-07 20:21:19 -0500

    update

--- src/components/inputTag.vue
+++ src/components/inputTag.vue
@@ -1,13 +1,27 @@
 <script>
-
+export default{
+    data(){
+        return{
+            currentValue:"",
+            tags:[],
+        };
+    },
+    methods:{
+        handleKeyDown(e){
+            if(e.key === "Enter" && this.currentValue != ''){
+                this.tags.push(this.currentValue)
+            }
+        },
+    },
+};
 </script>
 
 <template>
 <div class="inputTag">
     <div class="tags">
-
+        <div class="tag" v-for="(tag, index) in tags" :key="index">{{tag}}</div>
     </div>
-    <input type="text"/>
+    <input type="text" v-model="currentValue" @keydown="handleKeyDown"/>
 
 </div>
 
